48b3c6132f87724bc23d146fc8b72b51ab76ce51
sukant43 <sukant98657@gmail.com>
2025-02-15 18:14:28 +0530

    resolved

--- frontend/src/Components/History/History.jsx
+++ frontend/src/Components/History/History.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-impo
 function History() {
   const [data, setData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
